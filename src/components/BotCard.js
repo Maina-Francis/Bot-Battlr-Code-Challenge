37108b41c24192ca21cfef38aca56b58b2f06883
Francis Maina <sebastianfrancix@gmail.com>
2022-10-04 06:51:54 +0300

    destructures props in BotCard component

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,14 +9,11 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard(props) {
+  const { bot, handleBot, deleteBot } = props;
   return (
     <div className="ui column">
-      <div
-        className="ui card"
-        key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
-      >
+      <div className="ui card" key={bot.id} onClick={() => handleBot(bot)}>
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
         </div>
@@ -47,9 +44,7 @@ function BotCard({ bot }) {
             <div className="ui center aligned segment basic">
               <button
                 className="ui mini red button"
-                onClick={() =>
-                  console.log("add code to connect event listener")
-                }
+                onClick={() => deleteBot(bot)}
               >
                 x
               </button>
