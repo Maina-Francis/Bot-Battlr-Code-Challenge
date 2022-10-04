064d8a94371103b5c8fcfab2c66d987bf8dd5adb
Francis Maina <sebastianfrancix@gmail.com>
2022-10-04 06:17:05 +0300

    adds YourBotArmy props

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
 
+  //useState
+  const [botArmy, setBotArmy] = useState([]);
+  const [bot, setBot] = useState([]);
+
+  //fetch data from API
+
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((resp) => resp.json())
+      .then((bot) => setBot(bot));
+  }, []);
+
   return (
     <div>
-      <YourBotArmy />
+      <YourBotArmy
+        botArmy={botArmy}
+        removeBot={removeBotFromTheArmy}
+        deleteBot={deleteBotPermanently}
+      />
       <BotCollection />
     </div>
-  )
+  );
 }
 
 export default BotsPage;
