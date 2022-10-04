62fefa6b18ce9485e61a756ec44ca1f6791a3ef1
Francis Maina <sebastianfrancix@gmail.com>
2022-10-04 07:10:22 +0300

    adds BotCollection component

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,15 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
-  // Your code here
+function BotCollection(props) {
+  const { bot, addBot, deleteBot } = props;
+
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {bot.map((bot) => {
+          return <BotCard bot={bot} handleBot={addBot} deleteBot={deleteBot} />;
+        })}
         Collection of all bots
       </div>
     </div>
